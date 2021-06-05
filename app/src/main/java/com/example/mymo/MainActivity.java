a2b7c9a344527235dab9141d8ef0e1b1dc0ba110
kroci126 <gavinthan01@student.ciputra.ac.id>
2021-06-05 13:02:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mymo/MainActivity.java
@@ -0,0 +1,29 @@
+    package com.example.mymo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+            getSupportActionBar().hide();
+
+            Handler h = new Handler();
+            h.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 2000);
+        }
+    }
---
+++ app/src/main/java/com/example/mymo/db/dbAccess.java
@@ -0,0 +1,73 @@
+package com.example.mymo.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.mymo.Memo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class dbAccess {
+    private SQLiteDatabase database;
+    private dbOpenHelper openHelper;
+    private static volatile dbAccess instance;
+
+    private dbAccess(Context context) {
+        this.openHelper = new dbOpenHelper(context);
+    }
+
+    public static synchronized dbAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new dbAccess(context);
+        }
+        return instance;
+    }
+
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    public void save(Memo memo) {
+        ContentValues values = new ContentValues();
+        values.put("date", memo.getTime());
+        values.put("memo", memo.getText());
+        database.insert(dbOpenHelper.TABLE, null, values);
+    }
+
+    public void update(Memo memo) {
+        ContentValues values = new ContentValues();
+        values.put("date", new Date().getTime());
+        values.put("memo", memo.getText());
+        String date = Long.toString(memo.getTime());
+        database.update(dbOpenHelper.TABLE, values, "date = ?", new String[]{date});
+    }
+
+    public void delete(Memo memo) {
+        String date = Long.toString(memo.getTime());
+        database.delete(dbOpenHelper.TABLE, "date = ?", new String[]{date});
+    }
+
+    public List getAllMemos() {
+        List memos = new ArrayList<>();
+        Cursor cursor = database.rawQuery("SELECT * From memo ORDER BY date DESC", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            long time = cursor.getLong(0);
+            String text = cursor.getString(1);
+            memos.add(new Memo(time, text));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return memos;
+    }
+}
